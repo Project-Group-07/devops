542ceddbb687e4b13a86016d07077afe593594fe
Viktor <40721425@live.napier.ac.uk>
2025-11-11 00:59:55 +0000

    App test data

---
+++ src/test/java/com/napier/sem/AppTest.java
@@ -0,0 +1,17 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    void appStarts() {
+        assertTrue(true);
+    }
+
+    @Test
+    void simpleMath() {
+        assertEquals(5, 2 + 3);
+    }
+}
